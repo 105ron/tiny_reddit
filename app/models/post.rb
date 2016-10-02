a7879d547d91332bdbfad88fe9cfc23d0c1269c9
rhys <rbrooker105@gmail.com>
2016-10-02 16:22:59 +1100

    User to post link. Add post to database for user 1

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,5 @@
 class Post < ApplicationRecord
+	belongs_to :user
 	validates :content, presence: true, length: { maximum: 2024, minimum: 20 }
 	validates :title, presence: true, length: { maximum: 100, minimum: 4 }
 end
